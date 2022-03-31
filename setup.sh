18ca9e26f25edad35e4879a4b02de616ddbc8342
Keith Reid <kwreid@geeqs.net>
2022-03-30 22:28:04 -0700

    Update setup.sh
    Changed script to be specific about the folders it runs the find/replace on so as to avoid corrupting pack files in the .git hidden subfolders.
    (This may be a windows-only issue described in issue #1)

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-find . -type f -exec sed -i 's/{dockerHubUsername}/'$1'/g' {} +
+find ./arc -type f -exec sed -i 's/{dockerHubUsername}/'$1'/g' {} +
+find ./argo -type f -exec sed -i 's/{dockerHubUsername}/'$1'/g' {} +
+find ./flagger -type f -exec sed -i 's/{dockerHubUsername}/'$1'/g' {} +
+find ./flux -type f -exec sed -i 's/{dockerHubUsername}/'$1'/g' {} +
+find ./terraform -type f -exec sed -i 's/{dockerHubUsername}/'$1'/g' {} +
